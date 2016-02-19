9a6f81bdaee1b826ea3a9615f90f492d73df4b91
Travis <numbers1311407@gmail.com>
2016-02-19 03:47:39 -0500

    start refactor of state out of scope

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,10 @@
 
   var Murloc = (function () {
     function Murloc (attrs) {
+      if ('string' === typeof attrs) {
+        attrs = Murloc.murlocs[attrs];
+      }
+
       for (var attr in attrs) {
         if (attrs.hasOwnProperty(attr)) {
           this[attr] = attrs[attr];
@@ -11,9 +15,9 @@
     }
 
     var attack = 
-      Murloc.prototype.attack = function (onboard, wl, go, total, summoned) {
+      Murloc.prototype.attack = function (onboard, data) {
         if (!onboard && !this.charge) return 0;
-        return this.power + (2 * wl) + go;
+        return this.power + (2 * data.w) + data.g;
       };
 
     Murloc.murlocs = {
@@ -22,9 +26,9 @@
         power: 1,
         deckImg: 'assets/images/bars/murloc-tidecaller.png',
         cardImg: 'assets/images/cards/tidecaller.png',
-        attack: function (onboard, wl, go, total, summoned) {
-          var retv = attack.call(this, onboard, wl, go);
-          return onboard ? retv + summoned : retv;
+        attack: function (onboard, data) {
+          var retv = attack.call(this, onboard, data);
+          return onboard ? retv + data.summoned : retv;
         }
       },
       g: { 
@@ -32,8 +36,9 @@
         power: 1,
         deckImg: 'assets/images/bars/grimscale-oracle.png',
         cardImg: 'assets/images/cards/oracle.png',
-        attack: function (onboard, wl, go) {
-          return attack.call(this, onboard, wl, go - 1);
+        attack: function (onboard, data) {
+          data.g -= 1;
+          return attack.call(this, onboard, data);
         }
       },
       b: { 
@@ -48,8 +53,9 @@
         power: 3,
         deckImg: 'assets/images/bars/murloc-warleader.png',
         cardImg: 'assets/images/cards/warleader.png',
-        attack: function (onboard, wl, go) {
-          return attack.call(this, onboard, wl - 1, go);
+        attack: function (onboard, data) {
+          data.w -= 1;
+          return attack.call(this, onboard, data);
         }
       },
       m: {
@@ -58,8 +64,8 @@
         power: 2,
         deckImg: 'assets/images/bars/old-murk-eye.png',
         cardImg: 'assets/images/cards/murkeye.png',
-        attack: function (onboard, wl, go, total) {
-          return attack.call(this, onboard, wl, go, total) + total - 1;
+        attack: function (onboard, data) {
+          return attack.call(this, onboard, data) + data.total - 1;
         }
       },
       o: {
@@ -74,136 +80,223 @@
   }());
 
 
-  app.controller('IndexCtrl', ['$scope', function (scope) {
+  function State (board, graveyard, opponent) {
+    this.board = angular.copy(board);
+    this.graveyard = {raw: angular.copy(graveyard)};
+    this.opponent = angular.copy(opponent);
 
-    scope.combinationCap = root.helpers.combinationCap;
+    this.initGraveyard();
+    this.buildSets();
+    this.buildMetrics();
+    this.buildGraphData();
+  }
 
-    scope.murlocs = Murloc.murlocs;
+  State.prototype.free = function () {
+    return 7 - this.board.length;
+  };
 
-    scope.update = function () {
-      var free = scope.free();
-      var gy = _.reduce(scope.graveyard, function (o, val, key) {
-        // there's no reason to calculate past the # of free spaces
-        // if (val > free) val = free;
-        for (var i=0; i<val; i++) {
-          o.push(key);
-        };
-        return o;
-      }, []);
+  State.prototype.initGraveyard = function () {
+    this.graveyard.list = _.reduce(this.graveyard.raw, function (o, val, key) {
+      for (var i=0; i<val; i++) { o.push(key); };
+      return o;
+    }, []);
 
-      var gySets = root.helpers.combinations(gy, free);
+    this.graveyard.combos = 
+      root.helpers.combinations(this.graveyard.list, this.free());
 
-      scope.limitReached = gySets.exception ? true : false;
+    this.graveyard.summoned = 
+      this.graveyard.combos[0] ? this.graveyard.combos[0].length : 0;
 
-      var tsetslen = scope.totalCombinations = gySets.length;
-
-      gySets = _.reduce(gySets, function (o, arr) {
+    this.graveyard.data = 
+      _.reduce(this.graveyard.combos, function (o, arr) {
         var key = arr.sort().join('');
         o[key] || (o[key] = {count: 0});
         o[key].count += 1;
-        o[key].murlocs = arr;
+        o[key].set = arr;
         o[key].key = key;
         return o;
       }, {});
+  };
 
-      var gyValues = _.values(gySets);
+  /**
+   *
+   * param [Object] onboard - an array of murloc data objects representing the board
+   * param Object combo - data about a particular graveyard summon combo
+   * @api private
+   */
+  State.prototype.addSet = function (onboard, combo) {
+    var set = {};
+    var state = this;
 
-      scope.combinations = gyValues.length;
+    // start building the murloc board state data.  It begins with just the 
+    // opponents murlocs, and is build up as sets are added.  This is passed
+    // along to each murloc to determine their damage
+    var murlocs = {
+      summoned: this.graveyard.summoned,
+      total: this.opponent.o + this.opponent.w + this.opponent.g,
+      g: this.opponent.g,
+      w: this.opponent.w
+    };
 
-      var onboard = _.reject(scope.board, function (minion) {
-        return !minion.murloc;
-      });
-      var t = scope.opponent.o + scope.opponent.w + scope.opponent.g;
-      var go = scope.opponent.g;
-      var wl = scope.opponent.w;
+    _.each(onboard.concat(combo.set || []), function count(id) {
+      // again, account for board murloc objects vs ids
+      if (id.murloc) id = id.murloc;
 
-      scope.sets = []
+      // if it's an oracle, increment total oracles
+      if ('g' == id) murlocs.g += 1;
+      // if it's a warleader, incrememt warleaders
+      else if ('w' == id) murlocs.w += 1;
 
-      if (gyValues.length) {
-        for (var i=0; i<gyValues.length; i++) {
-          scope.sets.push(scope.buildSet(onboard, gyValues[i], wl, go, t));
-        }
-      } else if (onboard.length) {
-        scope.sets.push(scope.buildSet(onboard, {}, wl, go, t));
+      // then increment the total
+      murlocs.total += 1
+    });
+
+    var toMurloc = function (id, i, list) {
+      var power, m, onboard = false;
+      // Account for board murlocs, replacing obj with id and noting
+      // the updated power
+      if (id.murloc) {
+        // The combo.set, which is also passed through this function, is just
+        // an array of letter IDs, so if id.murloc is a property, this minion
+        // is "onboard".  This is passed to the attack function to designate
+        // primarily that the murloc doesn't need charge to do damage.
+        onboard = true;
+        power = id.power;
+        id = id.murloc;
       }
+      m = new Murloc(id);
 
-      scope.sets.sort(function (a, b) {
-        return a.damage - b.damage;
-      });
-
-      var min = scope.sets.min = scope.sets[0];
-      var max = scope.sets.max = scope.sets[scope.sets.length-1];
-
-      var avg = scope.sets.avg = max ? max.damage : 0;
-        
-      if (scope.sets.length > 1 && max.damage > 0) {
-        var setslen = scope.sets.length;
-        var maxdmg = max ? max.damage : 0;
-        var mindmg = min ? min.damage : 0;
-
-        var tsum = _.reduce(scope.sets, function (sum, set) {
-          return sum + set.count * set.damage;
-        }, 0);
-
-        avg = scope.sets.avg = tsum / tsetslen;
-
-        var n = _.chain(scope.sets)
-          .map(function (set) {
-            return Math.pow(set.damage - avg, 2);
-          })
-          .reduce(function (sum, sqv) {
-            return sum + sqv;
-          }, 0)
-          .value();
-
-        scope.sets.variance = n / scope.sets.length - 1;
-        scope.sets.sd = Math.sqrt(scope.sets.variance);
-
-        var byScore = _.reduce(scope.sets, function (o, set) {
-          o[set.damage] || (o[set.damage] = 0);
-          o[set.damage] += set.count;
-          return o;
-        }, {});
-
-        var data = _.reduce(_.range(mindmg, maxdmg+1), function (o, n) {
-          o.points.push({x: n, y: o.s/tsetslen});
-          o.s -= byScore[n] || 0;
-          return o;
-        }, {s: tsetslen, points: []});
-
-        scope.graphData = [{
-          key: "Probability",
-          values: data.points
-        }];
+      // if power was found, overwrite it on the murloc obj
+      if ('undefined' !== typeof power) {
+        m.power = power;
       }
-      else {
-        scope.graphData = [];
-      }
+      // then overwrite the attack function with an attack value
+      // calculated from the current state
+      m.attack = m.attack(onboard, angular.copy(murlocs));
+
+      return m;
     };
 
-    scope.opponent = {
-      w: 0,
-      g: 0,
-      o: 0
-    };
+    set.onboard = _.map(onboard, toMurloc);
+    set.onboard.damage = root.helpers.sum(set.onboard, 'attack');
 
-    scope.opponentDirty = function () {
-      return scope.opponent.w || scope.opponent.g || scope.opponent.o;
-    };
+    set.graveyard = _.map(combo.set, toMurloc);
+    set.graveyard.damage = root.helpers.sum(set.graveyard, 'attack');
 
-    scope.clearOpponent = function () {
-      scope.opponent.w = scope.opponent.g = scope.opponent.o = 0;
-    };
+    set.damage = set.onboard.damage + set.graveyard.damage;
 
-    scope.$watchGroup([
-      'opponent.w', 
-      'opponent.g', 
-      'opponent.o'
-    ], scope.update);
+    // unless there are no summons, the combo data will have a key
+    if (combo.key) {
+      set.count = combo.count;
+      set.key = combo.key;
+    }
 
+    this.sets.push(set);
+  };
+
+  State.prototype.buildSets = function () {
+    this.sets = [];
+
+    var onboard = _.reject(this.board, function (minion) {
+      return !minion.murloc;
+    });
+
+    if (this.graveyard.combos.length) {
+      _.each(this.graveyard.data, function (data, key) {
+        this.addSet(onboard, data);
+      }, this);
+    } 
+    else if (onboard.length) {
+      this.addSet(onboard, {});
+    }
+
+    this.sets.sort(function (a, b) {
+      return a.damage - b.damage;
+    });
+
+    this.sets.min = this.sets[0];
+    this.sets.max = this.sets[this.sets.length-1];
+    this.sets.total = this.graveyard.combos.length || 1;
+
+    this.sets.grouped = _.reduce(this.sets, function (o, set) {
+      o[set.damage] || (o[set.damage] = {count: 0, set: []});
+      o[set.damage].set.push(set);
+      o[set.damage].count += set.count;
+      return o;
+    }, {});
+  };
+
+  State.prototype.buildMetrics = function () {
+    this.metrics = {};
+
+    this.metrics.min = this.sets.min ? this.sets.min.damage : 0;
+    this.metrics.max = this.sets.max ? this.sets.max.damage : 0;
+      
+    if (this.sets.length > 1 && this.metrics.max > 0) {
+      this.metrics.sum = _.reduce(this.sets, function (sum, set) {
+        return sum + set.count * set.damage;
+      }, 0);
+
+      // actually calcualte the average (which was preset to 0)
+      this.metrics.avg = this.metrics.sum / this.sets.total;
+
+      this.metrics.variance = _.chain(this.sets)
+        .map(function (set) {
+          return Math.pow(set.damage - this.metrics.avg, 2);
+        }, this)
+        .reduce(function (sum, sqv) {
+          return sum + sqv;
+        }, 0)
+        .value() / this.sets.length - 1;
+
+      this.metrics.sd = Math.sqrt(this.metrics.variance);
+    }
+    else {
+      this.metrics.avg = this.metrics.min;
+    }
+  };
+
+  State.prototype.buildGraphData = function () {
+    var tCombos = this.sets.total;
+    var grouped = this.sets.grouped;
+
+    // Get a range of all possible values from the min score to the max score
+    var range = _.range(this.metrics.min, this.metrics.max+1);
+
+    // Iterate over the range, while subtracting the count of remaining combos
+    // (rCombos) with the given score along the way.  This gives the probability
+    // of each score being reached.  E.g say the possible scores are 
+    // [10,15,15,20].  We iterate from 10-20, passing the total combo count
+    // as rCombos.
+    //
+    // - score=10; rCombos=4 (total combo count)
+    //   - rCombos/tCombos == 4/4, 100% chance
+    // - score=11,12,13,14,15
+    //   - rCombos/tCombos == 3/4, 75% chance
+    // - score=16-20
+    //   - rCombos/tCombos == 1/4, 25% chance
+    //
+    var data = _.reduce(range, function (o, score) {
+      o.points.push({ x: score, y: o.rCombos/tCombos });
+      o.rCombos -= grouped[score] ? grouped[score].count : 0;
+      return o;
+    }, {rCombos: tCombos, points: []});
+
+    this.graphData = data.points.length
+      ? [{ key: "Probability", values: data.points }]
+      : [];
+  };
+
+
+  app.controller('IndexCtrl', ['$scope', function (scope) {
+    scope.combinationCap = root.helpers.combinationCap;
+    scope.murlocs = Murloc.murlocs;
+
+    //
+    // Board
+    //
+    
     scope.board = [];
-
-    scope.$watchCollection('board', scope.update);
 
     scope.onBoard = function () {
       return _.pluck(scope.board, 'murloc');
@@ -231,6 +324,88 @@
       scope.board = [];
     };
 
+    scope.incrPower = function (index) {
+      if (!scope.board[index]) return;
+      scope.board[index].power += 1;
+      scope.update();
+    };
+
+    scope.decrPower = function (index) {
+      if (!scope.board[index]) return;
+      scope.board[index].power = Math.max(scope.board[index].power - 1, 0);
+      scope.update();
+    };
+
+
+    //
+    // Opponent
+    //
+
+    scope.opponent = {
+      w: 0,
+      g: 0,
+      o: 0
+    };
+
+    scope.opponentDirty = function () {
+      return scope.opponent.w || scope.opponent.g || scope.opponent.o;
+    };
+
+    scope.clearOpponent = function () {
+      scope.opponent.w = scope.opponent.g = scope.opponent.o = 0;
+    };
+
+
+    //
+    // Graveyard
+    //
+
+    scope.graveyard = {
+      m: 0,
+      b: 0,
+      w: 0,
+      g: 0,
+      o: 0
+    };
+
+    scope.graveyardDirty = function () {
+      return scope.graveyard.m
+          || scope.graveyard.b
+          || scope.graveyard.w
+          || scope.graveyard.g
+          || scope.graveyard.o;
+    };
+
+    scope.clearGraveyard = function () {
+      scope.graveyard.m = 
+      scope.graveyard.b =
+      scope.graveyard.w =
+      scope.graveyard.g = 
+      scope.graveyard.o = 0;
+    };
+
+
+    //
+    // Combined
+    //
+
+    scope.dirty = function () {
+      return scope.opponentDirty() 
+        || scope.boardDirty() 
+        || scope.graveyardDirty();
+    };
+
+    scope.clear = function () {
+      scope.clearOpponent();
+      scope.clearBoard();
+      scope.clearGraveyard();
+    };
+
+    scope.update = function () {
+      scope.state = new State(scope.board, scope.graveyard, scope.opponent);
+      window.state = scope.state;
+    };
+
     scope.deckOptions = {
       murlocs: scope.murlocs,
       cards: [
@@ -245,67 +420,6 @@
           cardImg: 'assets/images/cards/recruit.png'
         }
       ],
-    };
-
-    scope.sets = [];
-    scope.combinations = 0;
-
-    var sum = scope.sum = function (arr, prop) {
-      return _.reduce(arr, function (mem, o) {
-        return mem + o[prop];
-      }, 0);
-    };
-
-    scope.buildSet = function (onboard, gyo, wl, go, total) {
-      var set = { };
-      var murlocs = gyo.murlocs || [];
-
-      var toMurloc = function (id, i, list) {
-        var power, m;
-        // account for board murlocs, replacing obj with id and noting
-        // the updated power
-        if (id.murloc) {
-          power = id.power;
-          id = id.murloc;
-        }
-        m = new Murloc(scope.murlocs[id]);
-        // if power was passed, overwrite it on the murloc obj
-        if ('undefined' !== typeof power) {
-          m.power = power;
-        }
-        // then overwrite the attack function with an attack value
-        // calculated from the current state
-        m.attack = m.attack(list.onboard, wl, go, total, murlocs.length);
-
-        return m;
-      };
-
-      _.each(onboard.concat(murlocs), function count(id) {
-        // again, account for board murloc objects vs ids
-        if (id.murloc) id = id.murloc;
-        // if it's an oracle, increment total oracles
-        if ('g' == id) go += 1;
-        // if it's a warleader, incrememt warleaders
-        else if ('w' == id) wl += 1;
-        total += 1;
-      });
-
-      onboard.onboard = true;
-
-      set.onboard = _.map(onboard, toMurloc);
-      set.onboard.damage = sum(set.onboard, 'attack');
-
-      set.graveyard = _.map(murlocs, toMurloc);
-      set.graveyard.damage = sum(set.graveyard, 'attack');
-
-      set.damage = set.onboard.damage + set.graveyard.damage;
-
-      if (gyo.key) {
-        set.count = gyo.count;
-        set.key = gyo.key;
-      }
-
-      return set;
     };
 
     scope.graphOptions = {
@@ -333,13 +447,13 @@
       }
     };
 
-    scope.graveyard = {
-      m: 0,
-      b: 0,
-      w: 0,
-      g: 0,
-      o: 0
-    };
+    // TODO these should all be in one watch if possible
+    //
+    scope.$watchGroup([
+      'opponent.w', 
+      'opponent.g', 
+      'opponent.o'
+    ], scope.update);
 
     scope.$watchGroup([
       'graveyard.m', 
@@ -349,45 +463,7 @@
       'graveyard.o'
     ], scope.update);
 
-    scope.graveyardDirty = function () {
-      return scope.graveyard.m
-          || scope.graveyard.b
-          || scope.graveyard.w
-          || scope.graveyard.g
-          || scope.graveyard.o;
-    };
-
-    scope.clearGraveyard = function () {
-      scope.graveyard.m = 
-      scope.graveyard.b =
-      scope.graveyard.w =
-      scope.graveyard.g = 
-      scope.graveyard.o = 0;
-    };
-
-    scope.incrPower = function (index) {
-      if (!scope.board[index]) return;
-      scope.board[index].power += 1;
-      scope.update();
-    };
-
-    scope.decrPower = function (index) {
-      if (!scope.board[index]) return;
-      scope.board[index].power = Math.max(scope.board[index].power - 1, 0);
-      scope.update();
-    };
-
-    scope.dirty = function () {
-      return scope.opponentDirty() 
-        || scope.boardDirty() 
-        || scope.graveyardDirty();
-    };
-
-    scope.clear = function () {
-      scope.clearOpponent();
-      scope.clearBoard();
-      scope.clearGraveyard();
-    };
+    scope.$watchCollection('board', scope.update);
   }]);
 
 
@@ -490,7 +566,7 @@
   });
 
   root.helpers = {};
-  root.helpers.combinationCap = 25000;
+  root.helpers.combinationCap = 30000;
 
   root.helpers.combinations = (function () {
     var fn = function(n, src, got, all) {
@@ -525,6 +601,12 @@
       return all;
     }
   })();
+
+  root.helpers.sum = function (arr, prop) {
+    return _.reduce(arr, function (mem, o) {
+      return mem + o[prop];
+    }, 0);
+  };
 
 
 
